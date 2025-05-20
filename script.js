b4bc9a5f8c037ee2a5890a1a8674c9d1b8ca860b
Juan Granados <garj980511@gmail.com>
2025-05-20 17:39:38 -0600

    datos

---
+++ script.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('.carousel').forEach(carousel => {
+    const images = carousel.querySelectorAll('.carousel-image');
+    let currentIndex = 0;
+
+    function showNextImage() {
+        images[currentIndex].classList.remove('active');
+        currentIndex = (currentIndex + 1) % images.length;
+        images[currentIndex].classList.add('active');
+    }
+
+    // Inicializar el primer elemento visible
+    images[currentIndex].classList.add('active');
+    setInterval(showNextImage, 3000); // Cambiar cada 3 segundos
+});
+
+function detenerCancion() {
+    var audio = document.getElementById("miAudio");
+    audio.pause();
+    audio.currentTime = 0;
+}
